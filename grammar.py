a878ef24852d258f01d67798ed08f0c820767db3
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2023-11-06 16:08:09 +0900

    feat: symbol_table 생성

--- grammar.py
+++ grammar.py
@@ -5,7 +5,7 @@ from symbol_table import SymbolTable
 class Parser:
     def __init__(self, lexer):
         self.lexer = lexer
-        self.symbol.table = SymbolTable
+        self.symbol_table = SymbolTable()
         
     def error(self, message):
         print(f"Error : {message}")
@@ -39,59 +39,81 @@ class Parser:
         ident_name = self.ident()
         self.assignment_op()
         value = self.expression()
-        
         self.symbol_table.set(ident_name, value)
+        print(ident_name)
+        print(self.symbol_table.get(ident_name))
         
     # <expression> → <term><term_tail>
     def expression(self):
         print("expression")
-        self.term()
-        self.term_tail()
+        value = self.term()
+        value += self.term_tail()
+        return value
         
     # <term_tail> → <add_op><term><term_tail> | ε
     def term_tail(self):
         print("term_tail")
+        value = 0
         if self.lexer.next_token == ADD_OP:
-            self.add_operator()
-            self.term()
-            self.term_tail()
+            isAddOp = self.add_operator()
+            value = self.term()
+            if(isAddOp == 1):
+                value += self.term_tail()
+            elif(isAddOp == -1):
+                value -= self.term_tail()            
+        return value
 
     # <term> → <factor> <factor_tail>
     def term(self):
         print("term")
-        self.factor()
-        self.factor_tail()
+        value = self.factor()
+        value *= self.factor_tail()
+        return value
 
     # <factor_tail> → <mult_op><factor><factor_tail> | ε
     def factor_tail(self):
         print("factor_tail")
         if self.lexer.next_token == MULT_OP:
-            self.mult_operator()
-            self.factor()
-            self.factor_tail()
+            isMultOp = self.mult_operator()
+            value = self.factor()
+            if isMultOp == 1:
+                value *= self.factor_tail()
+            elif isMultOp == -1:
+                value /= self.factor_tail()
+        else: 
+            value = 1
+        return value
 
     # <factor> → <left_paren><expression><right_paren> | <ident> | <const>
     def factor(self):
         print("factor")
         if self.lexer.next_token == LEFT_PAREN:
             self.match(LEFT_PAREN)
-            self.expression()
+            value = self.expression()
             self.match(RIGHT_PAREN)
-        elif self.lexer.next_token == IDENT:
-            self.ident()
+        elif self.lexer.next_token == IDENT:            
+            value = self.ident()
         else:
-            self.const()
+            value = self.const()
+        return value
         
 
     # <const> → any decimal numbers
     def const(self):
         print("const")
+        value = self.lexer.token_string
         self.match(CONST)
+        try:
+            return int(value)
+        except ValueError:
+            return float(value)
 
     # <ident> → any names conforming to C identifier rules
     def ident(self):
         print("ident")
+        ident_name = self.lexer.token_string
         self.match(IDENT)
+        return ident_name
 
     # <assignment_op> → :=
     def assignment_op(self):
@@ -108,8 +130,10 @@ class Parser:
         print("add_operator")
         if self.lexer.next_token == ADD_OP:
             self.match(ADD_OP)
+            return 1
         elif self.lexer.next_token == MIN_OP:
             self.match(MIN_OP)
+            return -1
         else:
             self.error("Expected a multiplication or division operator")
 
@@ -118,8 +142,10 @@ class Parser:
         print("mult_operator")
         if self.lexer.next_token == MULT_OP:
             self.match(MULT_OP)
+            return 1
         elif self.lexer.next_token == DIV_OP:
             self.match(DIV_OP)
+            return -1
         else:
             self.error("Expected a multiplication or division operator")
 
@@ -137,125 +163,3 @@ class Parser:
         print("parse")
         self.program()
         self.match(EOF)
-
-
-
-
-        
-
-   
-    
-    
-
-
-
-# #####
-
-# # <program> → <statements>
-# def program():
-#     statements()
-    
-# # <statements> → <statement> | <statement><semi_colon><statements>
-# def statements():
-#     statement()
-#     if next_token == SEMI_COLON:
-#         match(SEMI_COLON)
-
-# # <statement> → <ident><assignment_op><expression>
-# def statement():
-#     ident()
-#     assignment_op()
-#     expression()
-
-# # <expression> → <term><term_tail>
-# def expression():
-#     term()
-#     term_tail()
-
-# # <term_tail> → <add_op><term><term_tail> | ε
-# def term_tail():
-#     if next_token == ADD_OP:
-#         add_operator()
-#         term()
-#         term_tail()
-
-# # <term> → <factor> <factor_tail>
-# def term():
-#     factor()
-#     factor_tail()
-
-# # <factor_tail> → <mult_op><factor><factor_tail> | ε
-# def factor_tail():
-#     if next_token == MULT_OP:
-#         mult_operator()
-#         factor()
-#         factor_tail()
-
-# # <factor> → <left_paren><expression><right_paren> | <ident> | <const>
-# def factor():
-#     if next_token == LEFT_PAREN:
-#         match(LEFT_PAREN)
-#         expression()
-#         match(RIGHT_PAREN)
-#     elif next_token == IDENT:
-#         ident()
-#     else:
-#         const()
-
-# # <const> → any decimal numbers
-# def const():
-#     match(CONST)
-
-# # <ident> → any names conforming to C identifier rules
-# def ident():
-#     match(IDENT)
-
-# # <assignment_op> → :=
-# def assignment_op():
-#     match(ASSIGNMENT_OP)
-
-# # <semi_colon> → ;
-# def semi_colon():
-#     match(SEMI_COLON)
-
-# # <add_operator> → + | -
-# def add_operator():
-#     if next_token == ADD_OP:
-#         match(ADD_OP)
-#     elif next_token == MIN_OP:
-#         match(MIN_OP)
-#     else:
-#         error("Expected a multiplication or division operator")
-
-# # <mult_operator> → * | /
-# def mult_operator():
-#     if next_token == MULT_OP:
-#         match(MULT_OP)
-#     elif next_token == DIV_OP:
-#         match(DIV_OP)
-#     else:
-#         error("Expected a multiplication or division operator")
-
-# # <left_paren> → (
-# def left_paren():
-#     match(LEFT_PAREN)
-    
-# # <right_paren> → )
-# def right_paren():
-#     match(RIGHT_PAREN)
-
-
-
-# def match(expected_token):
-#     global next_token
-    
-#     # 현재 토큰이 expected_token과 일치하는지 확인
-#     if next_token == expected_token:
-#         lexical_analyzer.lexical()
-#     else:
-#         print(f"Unexpected token : {next_token}")
-    
-
-# def error(message):
-#     print(f"Error : {message}")
-#     sys.exit(1)
